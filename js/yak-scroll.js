be984847bc380ace9fd46b32adc8e449231db929
timdrake777 <78616250+timdrake777@users.noreply.github.com>
2021-06-23 00:12:07 +0400

    Mobile v2.0

--- js/yak-scroll.js
+++ js/yak-scroll.js
@@ -1,3 +1,26 @@
-$('.plus-btn').click(function (e) {
-	$('.project-points').toggleClass('accardion');
+var isMobile = false;
+
+$(document).ready( function() {
+
+	if($('.container').width() <= 375) {
+		isMobile = true;
+	}
+
+	if(isMobile){
+		$('.project-points-block-2').hide();
+		$('.project-points-block-3').hide();
+
+		$('.plus-btn-1').click(function (e) {
+			$('.project-points-block-1').slideToggle();
+		})
+
+		$('.plus-btn-2').click(function (e) {
+			$('.project-points-block-2').slideToggle();
+		})
+
+		$('.plus-btn-3').click(function (e) {
+			$('.project-points-block-3').slideToggle();
+		})
+	}
 })
+
