62d37b01ddd02e199fea12b45069d2eabc20f9d5
timdrake777 <78616250+timdrake777@users.noreply.github.com>
2021-06-22 18:34:51 +0400

    Changes at mobile version

--- js/yak-scroll.js
+++ js/yak-scroll.js
@@ -1,10 +1,8 @@
-<script type="text/javascript">
- $(document).ready(function(){
-    $("#menu").on("click","a", function (event) {
+$(document).ready(function(){
+	$("#menu").on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 3000);
     });
 });
-</script>
