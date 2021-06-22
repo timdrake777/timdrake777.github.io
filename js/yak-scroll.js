e64ec2f74edf12c532b701c595fe66eede0d8eb1
timdrake777 <78616250+timdrake777@users.noreply.github.com>
2021-06-22 18:53:39 +0400

    Add down-scale

--- js/yak-scroll.js
+++ js/yak-scroll.js
@@ -1,8 +1,3 @@
-$(document).ready(function(){
-	$("#menu").on("click","a", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 3000);
-    });
-});
+$('.plus-btn').click(function (e) {
+	$('.project-points').toggleClass('accardion');
+})
