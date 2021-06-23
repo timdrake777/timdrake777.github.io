adca808d726a3abe60fc91757222ff9c4aa14fd8
timdrake777 <78616250+timdrake777@users.noreply.github.com>
2021-06-23 19:12:37 +0400

    Mobile v2.7

--- js/yak-scroll.js
+++ js/yak-scroll.js
@@ -1,3 +1,18 @@
+$.fn.toggleAttrVal = function(attr, val1, val2) {
+    var test = $(this).attr(attr);
+    if ( test === val1) {
+      $(this).attr(attr, val2);
+      return this;
+    }
+    if ( test === val2) {
+      $(this).attr(attr, val1);
+      return this;
+    }
+    // default to val1 if neither
+    $(this).attr(attr, val1);
+    return this;
+ };
+
 var isMobile = false;
 
 $(document).ready( function() {
@@ -9,16 +24,21 @@ $(document).ready( function() {
 	if(isMobile){
 		$('.project-points-block-2').hide();
 		$('.project-points-block-3').hide();
+		$('.icon rect:nth-child(1)').attr('transform', 'rotate(0 4.79993 12)');
 
-		$('.plus-btn-1').click(function (e) {
-			$('.project-points-block-1').slideToggle();
-		})
+		$('.btn-section-1').on("click",(function (e) {
+			$('.icon rect:nth-child(1)').toggleAttrVal('transform', 'rotate(-90 4.79993 12)', 'rotate(0 4.79993 12)');
+			$('.project-points-block-1').slideToggle();  
+		}));
+		
 
-		$('.plus-btn-2').click(function (e) {
+		$('.btn-section-2').click(function (e) {
+			$('.icon-2 rect:nth-child(1)').toggleAttrVal('transform', 'rotate(0 4.79993 12)', 'rotate(-90 4.79993 12)');
 			$('.project-points-block-2').slideToggle();
 		})
 
-		$('.plus-btn-3').click(function (e) {
+		$('.btn-section-3').click(function (e) {
+			$('.icon-3 rect:nth-child(1)').toggleAttrVal('transform', 'rotate(0 4.79993 12)', 'rotate(-90 4.79993 12)');
 			$('.project-points-block-3').slideToggle();
 		})
 	}
